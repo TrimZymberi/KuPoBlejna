9fb27b978b051dfeab9832d7ef278af11e85de2b
trimzymberi <tz58110@ubt-uni.net>
2024-04-16 02:28:45 +0200

    Slider and Testimonials

--- kupoblejna/src/components/Banner.jsx
+++ kupoblejna/src/components/Banner.jsx
@@ -22,27 +22,32 @@ const Banner = () => {
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                   />
                    <input
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                   />
                   <input
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                     checked
                   />
                   <input
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                   />
                   <input
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                   />
                   
                 </div>
@@ -62,27 +67,32 @@ const Banner = () => {
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                   />
                    <input
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                   />
                   <input
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                     checked
                   />
                   <input
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                   />
                   <input
                     type="radio"
                     name="rating-2"
                     className="mask mask-star-2 bg-yellow-500"
+                    readOnly
                   />
                   
                 </div>
